a77d42c447dd7f1f4a953e82047eafeaddf5589b
3hreeman <santapemagic@gmail.com>
2024-10-08 19:28:29 +0900

    Obstacle tag added hit check logic change - OnTrigger2D -> OnCollision2D RigidBody2D Added to ShotObject

--- threeman_pf/Assets/Scripts/Poolable/ShotObject.cs
+++ threeman_pf/Assets/Scripts/Poolable/ShotObject.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ShotObject : PoolingObject {
     [SerializeField]
@@ -57,8 +58,49 @@ public class ShotObject : PoolingObject {
         shot_transform.right = dir;
     }
     
-    private void OnTriggerStay2D(Collider2D col) {
+    /*private void OnTriggerStay2D(Collider2D col) {
         if (!isAlive) return;
+        
+        if (col.gameObject.CompareTag("Obstacle")) {
+            var hitFx = ObjectPoolManager.instance.Get<SimpleHitFxObject>("fx_hit");
+            hitFx.transform.position = transform.position;
+            hitFx.SetDirection(owner.transform.position - transform.position);
+            ShotEnd();
+        }
+        if(curLife > 0) {
+            if (ownerType == UnitBase.UnitType.Player) {
+                if (col.gameObject.CompareTag("EnemyUnit")) {
+                    var enemy = col.gameObject.GetComponent<EnemyUnit>();
+                    enemy.TakeDmg(shotDmg);
+                    var hitFx = ObjectPoolManager.instance.Get<SimpleHitFxObject>("fx_hit");
+                    hitFx.transform.position = transform.position;
+                    hitFx.SetDirection(owner.transform.position - transform.position);
+                    curLife--;
+                } 
+            }
+            else if (ownerType == UnitBase.UnitType.Enemy) {
+                if (col.gameObject.CompareTag("PlayerUnit")) {
+                    var player = col.gameObject.GetComponent<PlayerUnit>();
+                    player.TakeDmg(shotDmg);
+                    curLife--;
+                }
+            }
+            
+            if(curLife <= 0) {
+                ShotEnd();
+            }
+        }
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D col) {
+        if (!isAlive) return;
+        
+        if (col.gameObject.CompareTag("Obstacle")) {
+            var hitFx = ObjectPoolManager.instance.Get<SimpleHitFxObject>("fx_hit");
+            hitFx.transform.position = transform.position;
+            hitFx.SetDirection(owner.transform.position - transform.position);
+            ShotEnd();
+        }
         
         if(curLife > 0) {
             if (ownerType == UnitBase.UnitType.Player) {
